acd20d6d79cd375380c60dc77b6728a27bfe79ee
DaniTox <danitox@protonmail.com>
2019-07-04 02:03:56 +0200

    added open from file functionality. Still to test deepy

--- iRingTunes Pro/Controllers/HomeVC.swift
+++ iRingTunes Pro/Controllers/HomeVC.swift
@@ -8,18 +8,15 @@
 
 import UIKit
 import MediaPlayer
+import MobileCoreServices
 
 class HomeVC: UIViewController {
-
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        
-        homeView.createButton?.addTarget(self, action: #selector(createRingtone), for: .touchUpInside)
-        homeView.managerButton?.addTarget(self, action: #selector(openManager), for: .touchUpInside)
+        homeView.createButton?.addTarget(self, action: #selector(createButtonPressed(_:)), for: .touchUpInside)
+        homeView.managerButton?.addTarget(self, action: #selector(openManagerButtonPressed), for: .touchUpInside)
     }
 
     var homeView = HomeView()
@@ -32,7 +29,46 @@ class HomeVC: UIViewController {
         return false
     }
     
-    @objc private func createRingtone() {
+    @objc private func createButtonPressed(_ sender: UIButton) {
+        //DA LOCALIZZARE
+        let alert = UIAlertController(title: "Source", message: "Da qualche sorgente vuoi prendere la musica?", preferredStyle: .actionSheet)
+        let action1 = UIAlertAction(title: "Files", style: .default) { (action) in
+            self.showFilesViewer()
+        }
+        let action2 = UIAlertAction(title: "Music Library", style: .default) { (action) in
+            DispatchQueue.main.async {
+                self.showMusicPicker()
+            }
+        }
+        
+        alert.addAction(action1)
+        alert.addAction(action2)
+        alert.addAction(UIAlertAction(title: "Annulla", style: .cancel, handler: nil))
+        
+        alert.popoverPresentationController?.sourceView = sender
+        alert.popoverPresentationController?.sourceRect = CGRect(x: sender.bounds.midX, y: sender.bounds.minY, width: 0, height: 0)
+        alert.popoverPresentationController?.permittedArrowDirections = .up
+        
+        present(alert, animated: true)
+        
+    }
+
+    private func showFilesViewer() {
+        let types: [String] = [
+            kUTTypeMP3 as String,
+            kUTTypeMPEG4Audio as String
+        ]
+        let vc = UIDocumentPickerViewController(documentTypes: types, in: .import)
+        vc.delegate = self
+        if #available(iOS 11.0, *) {
+            vc.allowsMultipleSelection = false
+        } else {
+            // Fallback on earlier versions
+        }
+        self.present(vc, animated: true)
+    }
+    
+    private func showMusicPicker() {
         let picker = MPMediaPickerController(mediaTypes: .anyAudio)
         picker.delegate = self
         picker.allowsPickingMultipleItems = false
@@ -42,8 +78,8 @@ class HomeVC: UIViewController {
         
         present(picker, animated: true, completion: nil)
     }
-
-    @objc private func openManager() {
+    
+    @objc private func openManagerButtonPressed() {
         let vc = RTManagerVC()
         let nav = UINavigationController(rootViewController: vc)
         present(nav, animated: true, completion: nil)
@@ -73,5 +109,28 @@ extension HomeVC: MPMediaPickerControllerDelegate {
     
     func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension HomeVC: UIDocumentPickerDelegate {
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentAt url: URL) {
+        let songName = url.lastPathComponent
+        
+        let vc = EditorVC()
+        vc.songName = songName
+        vc.selectedSongUrl = url
+
+        let nav = UINavigationController(rootViewController: vc)
+        
+//        dismiss(animated: true) {
+            DispatchQueue.main.async {
+                self.present(nav, animated: true)
+            }
+//        }
+        
     }
 }
